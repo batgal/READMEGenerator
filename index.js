6bd7ca2369e5784bb69dca7e0cb1a407c4c521de
KatlynGreen <batgalkgg@gmail.com>
2022-09-14 23:49:05 -0400

    Addition of .gitignore and table of contents

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generateHTML = ({ name, location, github, linkedin }) =>
+const generateHTML = ({
+  project,
+  description,
+  use,
+  install,
+  github,
+  linkedin,
+  license,
+  contributing,
+  contributing1,
+  contributing2,
+}) =>
   `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,12 +24,26 @@ const generateHTML = ({ name, location, github, linkedin }) =>
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+    <h1 class="display-4">Project Title: ${project}</h1>
+    <p class="lead">Description: ${description}</p>
+    <h3>README <span class="badge badge-secondary">Generator</span></h3>
+    <ul class="list-group"> <h2>Table of Contents</h2>
+      <li class="list-group-item"><a href="#installation">Installation</a></li>
+      <li class="list-group-item"><a href="#usage">Usage</a></li>
+      <li class="list-group-item"><a href="#license">License</a></li>
+      <li class="list-group-item"><a href="#contributing">Contributing to READMEGenerator</a></li> 
+      <li class="list-group-item"><a href="#contributing1">Github Flow for Pull Requests</a></li>
+      <li class="list-group-item"><a href="#contributing2">Resolving Bugs</a></li>
+      
+      
+      <li class="list-group-item"><li class="list-group-item" id="installation"><h2>Installation Guidelines</h2>${install}</li>
+      <li class="list-group-item"><li class="list-group-item" id="usage"><h2>Usage</h2>${use}</li>
+      <li class="list-group-item"><li class="list-group-item" id="license"><h2>License</h2> ${license}</a></li>
+      <li class="list-group-item"><li class="list-group-item" 
+      id="contributing"><h2>Contributing to READMEGenerator</h2>${contributing}</li><li class="list-group-item" id="contributing1"><h2>Github Flow for Pull Requests</h2>${contributing1}</li><li class="list-group-item" id="contributing2"><h2>Resolving Bugs</h2>${contributing2}</li></li>
+      <li class="list-group-item"><a href="#github">My GitHub username is ${github}</a></li>
+      <li class="list-group-item"><a href="#linkedin">LinkedIn: ${linkedin}</a></li>
+   
     </ul>
   </div>
 </div>
@@ -29,23 +54,48 @@ inquirer
   .prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?",
+      name: "project",
+      message: "What is your project title?",
     },
     {
       type: "input",
-      name: "location",
-      message: "Where are you from?",
+      name: "description",
+      message: "What is a description of your project?",
     },
     {
       type: "input",
-      name: "hobby",
-      message: "What is your favorite hobby?",
+      name: "install",
+      message: "What are the installation instructions for your project?",
     },
     {
       type: "input",
-      name: "food",
-      message: "What is your favorite food?",
+      name: "use",
+      message: "What is the usage information for your project?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "What licensing are you utilizing for the project?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message: "What is the overview of contributing guidelines?",
+    },
+    {
+      type: "input",
+      name: "contributing1",
+      message: "What is the Github flow for pull requests?",
+    },
+    {
+      type: "input",
+      name: "contributing2",
+      message: "What is the protocol for resolving bugs?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "What is the protocol for resolving bugs?",
     },
     {
       type: "input",
